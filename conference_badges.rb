caf7605f00243dd28a6148c4f9fe7e6a2e84eeeb
Matt Puchyr <matt.puchyr@gmail.com>
2019-09-09 22:37:24 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,3 +2,11 @@
 def badge_maker(name)
   "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(names)
+  badges = []
+  names.each do |name|
+    badges.push(badge_maker(name))
+  end
+  badges
+end
